0e4830a68dbf0091e30ed16a21033416052c475b
Daniel <danleavitt0@gmail.com>
2016-03-23 14:44:43 -0700

    add sensor mode

---
+++ test/ev3Test.js
@@ -0,0 +1,70 @@
+var test = require('tape')
+var WS = require('ws').Server
+var port = 3000
+var server = new WS({port: port})
+var ev3 = require('../lib/index')
+
+setTimeout(function () {
+  var run = ev3.robot('localhost:3000')
+  run(function * () {
+    yield ev3.move().forever(50, 0)
+    yield ev3.move().timed(2000, 50, 50)
+    yield ev3.move().rotations(1, 50, 50)
+  })
+}, 1000)
+
+function startRun (socket) {
+  return setInterval(function () {
+      socket.send(JSON.stringify({
+        type: 'sensor_read',
+        value: {
+          'b': {
+            type: 'motor',
+            value: 'running'
+          },
+          'c': {
+            type: 'motor',
+            value: 'running'
+          },
+          '1': {
+            type: 'touch',
+            value: '0'
+          },
+          '2': {
+            type: 'color',
+            value: 5
+          }
+        }
+      }))
+    }, 150)
+}
+
+server.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    console.log(msg)
+  })
+
+  var interval = startRun(socket)
+  setTimeout(function () {
+    clearInterval(interval)
+    setInterval(function () {
+      socket.send(JSON.stringify({
+        type: 'sensor_read',
+        value: {
+          'b': {
+            type: 'motor',
+            value: ''
+          },
+          'c': {
+            type: 'motor',
+            value: ''
+          },
+          '1': {
+            type: 'touch',
+            value: '0'
+          }
+        }
+      }))
+    })
+  }, 4000)
+})
